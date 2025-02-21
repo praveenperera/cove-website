0b0f1a8be674537aa1a90cb067ef0f70c9a2cb42
Praveen Perera <me@praveenperera.com>
2025-02-21 16:55:48 -0600

    Initial commit using TailwindUI Pocket template

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cove Website
+
+## Getting started
+
+```bash
+npm install
+```
+
+Next, run the development server:
+
+```bash
+npm run dev
+```
+
+Finally, open [http://localhost:3000](http://localhost:3000) in your browser to view the website.
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import { CallToAction } from '@/components/CallToAction'
+import { Hero } from '@/components/Hero'
+import { Features } from '@/components/Features'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <CallToAction />
+      <Features />
+    </>
+  )
+}
