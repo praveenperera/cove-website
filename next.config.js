d00be9d508b8398f5204f815b302441246d06f3d
Praveen Perera <me@praveenperera.com>
2025-03-07 09:31:37 -0600

    Add redirect to testflight

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/TestFlight',
+        destination: 'https://testflight.apple.com/join/pDxFQsxF',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
