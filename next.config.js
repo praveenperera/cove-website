c701186e2ab0103cb8f1bcd9bf91227e60be01ae
Praveen Perera <me@praveenperera.com>
2025-06-16 09:21:46 -0500

    Update testflight link to point to app store

--- next.config.js
+++ next.config.js
@@ -15,18 +15,20 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: '/TestFlight',
+        source: '/beta',
         destination: 'https://testflight.apple.com/join/pDxFQsxF',
         permanent: true,
       },
       {
         source: '/test-flight',
-        destination: 'https://testflight.apple.com/join/pDxFQsxF',
+        destination:
+          'https://apps.apple.com/app/cove-simple-bitcoin-wallet/id6642680364',
         permanent: true,
       },
       {
         source: '/testflight',
-        destination: 'https://testflight.apple.com/join/pDxFQsxF',
+        destination:
+          'https://apps.apple.com/app/cove-simple-bitcoin-wallet/id6642680364',
         permanent: true,
       },
     ]
