86634e5b82eb46ecdceacb60e2d49efead700376
Praveen Perera <me@praveenperera.com>
2025-06-17 08:49:17 -0500

    Add OpenGraph and Twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,12 +11,51 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://covewallet.com'),
   title: {
     template: '%s - Cove',
     default: 'Cove - The simple bitcoin only mobile wallet',
   },
   description:
     'Cove is a Bitcoin only mobile wallet that is simple to use, but feature rich.',
+  openGraph: {
+    title: 'Cove - The simple bitcoin only mobile wallet',
+    description:
+      'Cove is a Bitcoin only mobile wallet that is simple to use, but feature rich.',
+    url: 'https://covewallet.com',
+    siteName: 'Cove',
+    images: [
+      {
+        url: '/big.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Cove Bitcoin Wallet Logo',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cove - The simple bitcoin only mobile wallet',
+    description:
+      'Cove is a Bitcoin only mobile wallet that is simple to use, but feature rich.',
+    images: ['/big.jpg'],
+    creator: '@praveenperera',
+    site: '@praveenperera',
+  },
+  keywords: [
+    'cove',
+    'bitcoin',
+    'wallet',
+    'mobile',
+    'bitcoin only',
+    'self custody',
+    'pbst',
+    'hardware wallet support',
+  ],
+  authors: [{ name: 'Praveen Perera' }],
+  category: 'technology',
 }
 
 export default function RootLayout({
