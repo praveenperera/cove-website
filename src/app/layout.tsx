76d605564eeea1e1608ca0709a620daf6535cc7a
Praveen Perera <me@praveenperera.com>
2026-02-19 13:39:41 -0600

    Add Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import clsx from 'clsx'
 
 import '@/styles/tailwind.css'
@@ -74,7 +75,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={clsx('bg-gray-50 antialiased', inter.variable)}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
